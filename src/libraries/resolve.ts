f8b59b9726143218f345ae49660b608af73f16e9
范文华 <531362022@qq.com>
2019-11-29 10:55:59 +0800

    feat: Predefine resolve.alias

--- src/libraries/resolve.ts
+++ src/libraries/resolve.ts
@@ -4,6 +4,7 @@ export const setResolve = (resolve: Resolve): void => {
   resolve.extensions = [
     '.js', '.ts', '.tsx', '.jsx',
   ];
+  resolve.alias = {};
 
   // @see https://webpack.docschina.org/guides/build-performance/#%E8%A7%A3%E6%9E%90
   resolve.symlinks = false;
