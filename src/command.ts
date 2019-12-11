e704a334996394d96e37126769c3dc028081351c
范文华 <531362022@qq.com>
2019-12-11 10:54:59 +0800

    feat: File webpack.config.ts is built in now, user can run project without configure file

--- src/command.ts
+++ src/command.ts
@@ -1,6 +1,18 @@
 #!/usr/bin/env ts-node
 
+import fs from 'fs';
+import path from 'path';
+
 const option = process.argv.splice(2, 1)[0];
+
+if (process.argv.indexOf('--config') === -1) {
+  if (
+    !fs.existsSync(path.resolve('webpack.config.ts')) &&
+    !fs.existsSync(path.resolve('webpack.config.js'))
+  ) {
+    process.argv.push('--config', path.join(__dirname, 'misc', 'webpack.config.js'))
+  }
+}
 
 switch (option) {
   case 'start':
---
+++ src/misc/webpack.config.ts
@@ -0,0 +1,5 @@
+import webpackGenius from '../index';
+
+export default webpackGenius(8000, (/* genius */) => {
+  // genius.xxx().yyy().zzz()
+});
