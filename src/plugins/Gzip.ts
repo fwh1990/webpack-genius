62e7d1085502a9fab39196591a105b1ec1754d65
范文华 <531362022@qq.com>
2019-12-16 11:33:40 +0800

    fix: Gzip should only effect .js and .css extensions

--- src/plugins/Gzip.ts
+++ src/plugins/Gzip.ts
@@ -5,7 +5,13 @@ import { PluginHandle } from "./PluginHandle";
 export class Gzip extends PluginHandle {
   public collect(): Plugin[] {
     return [
-      new CompressionPlugin(),
+      new CompressionPlugin({
+        threshold: 8192,
+        test: [
+          /\.js(\?.*)?$/i,
+          /\.css(\?.*)?$/i,
+        ],
+      }),
     ];
   }
 }
